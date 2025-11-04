3af83e475b7e64abc6ad307e68c87953b902acf5
Md Fuadul Islam <fuad@Fuads-Mac.local>
2025-11-04 08:55:40 +0600

    create AiPlayer
    Signed-off-by: Md Fuadul Islam <fuad@Fuads-Mac.local>

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 class Player():
@@ -15,3 +16,16 @@ class RandomBot(Player):
 
     def get_move(self, game):
         return random.choice(game.available_moves())
+    
+class AiPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            return 4 # always pick the center of the board in the first move
+        else:
+            return self.minimax(game, self.letter)
+    
+    def minimax(self, game, player):
+        return 0
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -12,3 +12,50 @@ class TicTacToe():
             row = self.board[start:end]
             # ' | '.join(row) joins all elements in the row with " | " between them.
             print('| ' + ' | '.join(row) + ' |')
+
+    def available_moves(self):
+        ret = []
+        for i in self.board:
+            if i == ' ' :
+                ret.append(i)
+        return ret
+
+    def make_move(self, square, letter):
+        self.board[square] = letter
+        if self.is_winner(letter):
+            self.current_winner = letter
+
+    def is_winner(self, letter):
+        # check the rows
+        for i in range(3):
+            ret = True
+            for j in range(3):
+                if self.board[(i*3)+j] != letter:
+                    ret = False
+            if ret == True:
+                return ret
+        # check the columns
+        for i in range(3):
+            ret = True
+            for j in range(3):
+                if self.board[i+(j*3)] != letter:
+                    ret = False
+            if ret == True:
+                return ret
+        # check diagonals
+        ret = True
+        for i in self.board[0, 4, 8]:
+            if i != letter:
+                ret = False
+        if ret == True:
+            return ret
+        
+        # check second diagonals
+        ret = True
+        for i in self.board[2, 4, 6]:
+            if i != letter:
+                ret = False
+        if ret == True:
+            return ret
+        
+        return False
