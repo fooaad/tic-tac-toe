b3ded9f9e7075547fe1f2794f792b62bb64e9815
Md Fuadul Islam <fuad@Fuads-Mac.local>
2025-11-04 08:56:04 +0600

    implement minimax algorithm
    Signed-off-by: Md Fuadul Islam <fuad@Fuads-Mac.local>

--- player.py
+++ player.py
@@ -28,4 +28,37 @@ class AiPlayer(Player):
             return self.minimax(game, self.letter)
     
     def minimax(self, game, player):
-        return 0
+        max_player = self.letter # AiPlayer
+        other_player = 'O' if player == 'X' else 'X' # max_player can be equal to other_player
+
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}  # each score should maximize
+        else:
+            best = {'position': None, 'score': math.inf}  # each score should minimize
+
+        for possible_move in game.available_moves():
+            game.make_move(possible_move, player)
+            if game.is_winner: 
+                if player == max_player:
+                    score = 1 + len(game.available_moves())
+                else:
+                    score = - 1 - len(game.available_moves())
+            elif len(game.available_moves()) == 0:
+                score = 0
+            else:
+                score = self.minimax(game, other_player)
+
+            if player == max_player:
+                if score > best['score']:
+                    best['score'] = score
+                    best['position'] = possible_move
+            else:
+                if score < best['score']:
+                    best['score'] = score
+                    best['position'] = possible_move
+
+            #undo move
+            game.board[possible_move] = ' '
+            game.current_winner = None
+        
+        return best['position']
